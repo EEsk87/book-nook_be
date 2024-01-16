a5502a59f35b9b47a85e8bc88f3e9f9b0b03aa80
Earl Eskridge <EJay@Cindys-Air.lan>
2024-01-16 17:17:19 -0500

    added review class

---
+++ FullStackAuth_WebAPI_Starter-main/FullStackAuth_WebAPI/Models/Review.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Review
+    {
+
+
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string ReviewBody { get; set; }
+        public double Rating { get; set; }
+
+        
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+
+        
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
