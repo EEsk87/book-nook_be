1095d4cf5310cb7c97aa5ea3f50f542df22f0b6c
Earl Eskridge <EJay@Cindys-Air.lan>
2024-01-16 17:25:10 -0500

    Favorites class

---
+++ FullStackAuth_WebAPI_Starter-main/FullStackAuth_WebAPI/Models/favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace FullStackAuth_WebAPI.Models
+{
+	public class favorite
+	{
+		public int ID { get; set; }
+		public string BookId { get; set; }
+		public string Title { get; set; }
+		public string ThumbnailURL { get; set; }
+
+		public string UserId { get; set; }
+		public User user { get; set; }
+	}
+}
+
